a816c55dc46cda68d52cb647fe72126010080180
_yura <javadevelope@yandex.ru>
2022-07-23 10:55:50 +0300

    feat: friends-like

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,21 +1,34 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.error.NotFoundException;
+import ru.yandex.practicum.filmorate.error.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.use_case.FilmUseCase;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/films")
 public class FilmController {
+
+    private final FilmService service;
+
+    @Autowired
+    public FilmController(FilmService service) {
+        this.service = service;
+    }
 
     @PostMapping()
     public Film create(@RequestBody Film film) {
         if (film == null) {
             throw new RuntimeException("Пустой запрос");
         }
-        return FilmUseCase.createFilm(film);
+        return service.createFilm(film);
     }
 
     @PutMapping()
@@ -23,11 +36,31 @@ public class FilmController {
         if (film == null) {
             throw new RuntimeException("Пустой запрос");
         }
-        return FilmUseCase.updateFilm(film);
+        return service.updateFilm(film);
     }
 
     @GetMapping()
     public List<Film> get() {
-        return FilmUseCase.getFilms();
+        return service.getFilms();
+    }
+
+    @GetMapping("/{filmId}")
+    public Film getFilm(@PathVariable long filmId) {
+        return service.getFilm(filmId);
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public Film setLike(@PathVariable long id, @PathVariable long userId) {
+        return service.setLike(id,userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public Film deleteLike(@PathVariable long id, @PathVariable long userId) {
+        return service.deleteLike(id,userId);
+    }
+
+    @GetMapping("/popular")
+    public List<Film> getTopPopular(@RequestParam(value = "count", defaultValue = "10", required = false) String count) {
+        return service.getTopPopular(Integer.parseInt(count));
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,8 +1,9 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.use_case.UserUseCase;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.List;
 
@@ -10,12 +11,19 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService service;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
     @PostMapping()
     public User create(@RequestBody User user) {
         if (user == null) {
             throw new RuntimeException("Пустой запрос");
         }
-        return UserUseCase.createUser(user);
+        return service.createUser(user);
     }
 
     @PutMapping()
@@ -23,12 +31,37 @@ public class UserController {
         if (user == null) {
             throw new RuntimeException("Пустой запрос");
         }
-        return UserUseCase.updateUser(user);
+        return service.updateUser(user);
 
     }
 
     @GetMapping()
     public List<User> get() {
-        return UserUseCase.getUsers();
+        return service.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    public User getUser(@PathVariable long userId) {
+        return service.getUser(userId);
+    }
+
+    @PutMapping("/{id}/friends/{friendId}")
+    public User addToFriendList(@PathVariable long id, @PathVariable long friendId) {
+        return service.addToFriendList(id, friendId);
+    }
+
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public User deleteFormFriendList(@PathVariable long id, @PathVariable long friendId) {
+        return service.deleteFromFriendList(id, friendId);
+    }
+
+    @GetMapping("/{id}/friends")
+    public List<User> getFriends(@PathVariable long id) {
+        return service.getFriends(id);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public List<User> getCommonFriends(@PathVariable long id, @PathVariable long otherId) {
+        return service.getCommonFriends(id, otherId);
     }
 }
